795daa66787c246e80a6b50df292368e271008c9
Everts Rozenbaums <evertrozenbaum@gmail.com>
2026-02-27 16:13:23 +0200

    wip: storage

--- cmd/log.go
+++ cmd/log.go
@@ -1,27 +1,30 @@
 /*
 Copyright © 2026 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/spf13/cobra"
 )
 
 // logCmd represents the log command
 var logCmd = &cobra.Command{
-	Use:   "log",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "log PROJECT MINUTES [NOTE]",
+	Short: "Add a log entry to a project",
+	Long:  `Add a log entry to a project. Automatically adds the current time to the log.`,
+	Args:  cobra.RangeArgs(2, 3),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("log called")
+		projName := args[0]
+		minutes64, err := strconv.ParseUint(args[1], 10, 16)
+		if err != nil {
+			fmt.Printf("❌ Invalid minutes '%s': %v\n", args[1], err)
+			return
+		}
+		minutes := uint16(minutes64)
+
 	},
 }
 
--- project/project.go
+++ project/project.go
@@ -1,1 +1,29 @@
 package project
+
+import (
+	"strings"
+	"time"
+)
+
+type Project struct {
+	Name      string  `json:"name"`
+	Wage      float32 `json:"wage"`
+	TotalTime uint    `json:"total_time"`
+	Logs      []Log   `json:"logs"`
+}
+
+type Log struct {
+	Minutes uint
+	Note    string
+	Date    time.Time
+}
+
+func (p *Project) AddLog(minutes uint, note string) {
+	p.TotalTime += minutes
+
+	p.Logs = append(p.Logs, Log{
+		Minutes: minutes,
+		Note:    strings.TrimSpace(note),
+		Date:    time.Now(),
+	})
+}
